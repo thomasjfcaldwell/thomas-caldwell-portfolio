9147f0cab8e8380924fd432dc30f0285ba7cd28a
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2023-12-08 15:52:44 -0800

    adding theme

--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,14 @@ import Work from './Main/Work/Work';
 import Employment from './Main/Employment/Employment';
 import Skills from './Main/Skills/Skills';
 import Contact from './Main/Contact/Contact';
+import { Button } from '@chakra-ui/react';
 
 function Main() {
 	return (
 		<div className='main'>
 			<div>
 				<h2>Main</h2>
+				<Button bg='brand.900'>Click on me!</Button>
 				<Intro />
 				<Work />
 				<Employment />
---
+++ src/Theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+	colors: {
+		brand: {
+			100: '#000',
+			900: '#953553',
+		},
+	},
+});
+export default theme;
